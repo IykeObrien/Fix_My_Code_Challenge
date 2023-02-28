0a476aab9faa44175ce2f66ac9181ce20d9d90e2
IKECHUKWU OGUDU (CapShelters) <111122251+IykeObrien@users.noreply.github.com>
2023-02-27 19:44:55 -0600

    Update 2-sort.rb

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,20 +1,31 @@
-#include <string.h>
-#include <stdlib.h>
-#include "lists.h"
+###
+#
+#  Sort integer arguments (ascending)
+#
+###
 
-/**
- * free_dlistint - Free a list
- *
- * @head: A pointer to the first element of the list
- */
-void free_dlistint(dlistint_t *head)
-{
-	dlistint_t *node;
+result = []
+ARGV.each do |arg|
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
 
-	while (head)
-	{
-		node = head;
-		head = head->next;
-		free(node);
-	}
-}
+    # convert to integer
+    i_arg = arg.to_i
+
+    # insert result at the right position
+    is_inserted = false
+    i = 0
+    l = result.size
+    while !is_inserted && i < l do
+        if result[i] < i_arg
+            i += 1
+        else
+            result.insert(i, i_arg)
+            is_inserted = true
+            break
+        end
+    end
+    result << i_arg if !is_inserted
+end
+
+puts result
